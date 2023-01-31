2248efd21f896085e473bf5a0bd9078b675cf4a5
Thao Le <thaole0325@gmail.com>
2023-01-31 10:33:32 -0500

    deleted random code in admin.py file

--- app/admin.py
+++ app/admin.py
@@ -9,11 +9,3 @@ admin.site.register(MusicPost)
 admin.site.register(Song)
 admin.site.register(Comment)
 
-
-# === revising comment ==== #
-
-admin.register(Comment)
-class CommentAdmin(admin.ModelAdmin):
-    list_display = ('username', 'music_post', 'date_published')
-    list_filter = ('date_published', 'date_modified')
-    search_fields = ('username', 'body')
